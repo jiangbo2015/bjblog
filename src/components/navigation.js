28bd6df70a14c0eb978021e6de2875c3bcdaf72f
jiangbo2015 <772051431@qq.com>
2025-02-15 19:56:32 +0800

    feat: u

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -9,8 +9,8 @@ export default () => (
       </a>
       <div className={styles.menuWrap}>
         <a href="https://bojiangsoftware.com/develop/">软件开发</a>
-        <a href="https://bojiangsoftware.com/promotion/">数字推广</a>
-        <a href="https://bojiangsoftware.com/ecommerce/">线上电商</a>
+        <a href="https://bojiangsoftware.com/promotion/">线上推广</a>
+        <a href="https://bojiangsoftware.com/ecommerce/">北美电商</a>
         <a href="https://bojiangsoftware.com/template/">客户案例</a>
         <a href="https://bojiangsoftware.com/#about">关于我们</a>
         <a href="https://bojiangsoftware.com/contact/">联系我们</a>
